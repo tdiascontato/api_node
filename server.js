0061ebe12b4291abd95dbe781b36be6a26cd8121
Tiago Dias <tdiascontato@gmail.com>
2024-07-29 13:36:17 -0300

    last commit

--- server.js
+++ server.js
@@ -2,12 +2,20 @@
 const express = require('express');
 const createUserController = require("./controllers/userController");
 const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
 app.use(express.json());
+app.use(bodyParser.json());
+app.use(cors({
+    origin: 'http://localhost:4200', // Allow requests from this origin
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+}));
 
 app.get('/', (req, res) => {
-    res.send({'message': 'Lests GO!'})
+    res.send({'message': 'Lets GO!'})
 });
 
-app.post('/user/signup', createUserController);
+app.post('/signup', createUserController);
 
 module.exports = app;
